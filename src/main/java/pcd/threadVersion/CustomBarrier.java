dbbd5a7999f2ef2e4268058fb24e75b9924594d3
Filoferro03 <filippo.ferretti03@gmail.com>
2026-05-20 15:08:48 +0200

    Modified CustomBarrier to add generation

--- src/main/java/pcd/threadVersion/CustomBarrier.java
+++ src/main/java/pcd/threadVersion/CustomBarrier.java
@@ -4,23 +4,29 @@ public class CustomBarrier {
     private final int totalThreads;
     private int waitingThreads;
     private final Runnable barrierAction;
+    private int generation;
 
     public CustomBarrier(int totalThreads, Runnable barrierAction) {
         this.totalThreads = totalThreads;
         this.waitingThreads = totalThreads;
         this.barrierAction = barrierAction;
+        this.generation = 0;
     }
 
     public synchronized void await() throws InterruptedException {
+        int myGeneration = this.generation;
         waitingThreads--;
         if (waitingThreads == 0) {
             if (barrierAction != null) {
                 barrierAction.run();
             }
             waitingThreads = totalThreads;
+            generation++;
             notifyAll();
         } else {
-            wait();
+            while (myGeneration == this.generation) {
+                wait();
+            }
         }
     }
 }
